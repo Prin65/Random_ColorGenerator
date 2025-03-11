72465dc7a5406ecfa2d1de1ef6022f083f4a9c69
Prin65 <princy1227@gmail.com>
2025-03-11 16:05:59 +0530

    some changes in all three files with h1 tag

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const getColor = () =>{
     const randomCode = "#"+randomNumber.toString(16);
     document.body.style.backgroundColor=randomCode;
     document.getElementById("color-code").innerText=randomCode; 
+    document.querySelector("h1").innerText="Yup!! New Color";
 
     navigator.clipboard.writeText(randomCode);//copy to clipboard
     
